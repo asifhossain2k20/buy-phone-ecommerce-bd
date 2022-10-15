5b8805a63a5c260bf1392465832f82fcb44b50c3
asifhossain2k20 <asif.hossain2k20@gmail.com>
2022-10-15 11:14:58 +0600

    Show More Data

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-const loadPhone=async(searchText)=>{
+const loadPhone=async(searchText,datalimit)=>{
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res=await fetch(url);
     const data=await res.json();
-    displayPhone(data.data)
+    displayPhone(data.data,datalimit)
     
 }
 
-const displayPhone=phones=>{
+const displayPhone=(phones,dataLimit)=>{
     const phoneContainer=document.getElementById('phone-container')
     phoneContainer.textContent='';
 
-    //display 20 Phone Only
-    phones=phones.slice(0,10);
-
+    //display 10 Phone Only
+    const showAll=document.getElementById('show-all');
+    if(dataLimit && phones.length>10){
+        phones=phones.slice(0,10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+    
     //display no phone found 
     const noPhone=document.getElementById('no-found-message');
     if(phones.length===0){
@@ -42,12 +49,19 @@ const displayPhone=phones=>{
     toggleSpinner(false);
 }
 
-document.getElementById('btn-search').addEventListener('click',function(){
+const processSearch=(datalimit)=>{
     //start loader
     toggleSpinner(true);
     const inputField=document.getElementById('input-field');
     const searchValue=inputField.value;
-    loadPhone(searchValue);
+    loadPhone(searchValue,datalimit);
+}
+
+
+//handle search button click
+document.getElementById('btn-search').addEventListener('click',function(){
+    //start loader
+    processSearch(10);
 })
 
 const toggleSpinner=isLoading=>{
@@ -60,4 +74,10 @@ const toggleSpinner=isLoading=>{
     }
 }
 
-loadPhone('')
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    //start loader
+    processSearch();
+})
+
+
+// loadPhone('')
