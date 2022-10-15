58cc72dc072add31c43ee43a9b98c10c61a351d8
asifhossain2k20 <asif.hossain2k20@gmail.com>
2022-10-15 11:50:09 +0600

    Modal Added

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ const loadPhone=async(searchText,datalimit)=>{
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res=await fetch(url);
     const data=await res.json();
-    displayPhone(data.data,datalimit)
-    
+    displayPhone(data.data,datalimit) 
 }
 
 const displayPhone=(phones,dataLimit)=>{
@@ -37,8 +36,9 @@ const displayPhone=(phones,dataLimit)=>{
         <div class="card">
             <img src="${phone.image}" class="card-img-top p-4" alt="...">
             <div class="card-body">
-            <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">${phone.slug}</p>
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.slug}</p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#showPhoneModal">Show Details</button>
             </div>
         </div>
         `
@@ -64,6 +64,14 @@ document.getElementById('btn-search').addEventListener('click',function(){
     processSearch(10);
 })
 
+//search input field enter key handler
+document.getElementById('input-field').addEventListener('keypress',function(e){
+    if(e.key==='Enter'){
+        processSearch(10);
+    }
+})
+
+
 const toggleSpinner=isLoading=>{
     const loaderSection=document.getElementById('loader');
     if(isLoading){
@@ -79,5 +87,23 @@ document.getElementById('btn-show-all').addEventListener('click',function(){
     processSearch();
 })
 
+const loadPhoneDetails=async(id)=>{
+    const url=`https://openapi.programming-hero.com/api/phone/${id}`;
+    const res=await fetch(url);
+    const data=await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails=(phone)=>{
+    const modalTitle=document.getElementById('showPhoneModalLabel');
+    modalTitle.innerText=phone.name;
+    const phoneDetails=document.getElementById('phone-details');
+    phoneDetails.innerHTML=`
+        <p>Release Date : ${phone.releaseDate ? phone.releaseDate : 'No release Date Found'}</p>
+        <p>Storage : ${phone.mainFeatures ? phone.mainFeatures.storage :'No Info Found'} </p>
+        <p>Others : ${phone.others ? phone.others.Bluetooth : 'No BlueTooth Info'}
+
+    `
+}
 
 // loadPhone('')
